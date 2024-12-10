2c48b9cb04847cc94da7544d5bf457a06e68319c
Victor Tikhonov <victor.tix777@mail.ru>
2024-12-10 20:50:41 +0400

    Initial commit

---
+++ src/test/java/ru/victortikhonov/autoserviceapp/AutoserviceappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.victortikhonov.autoserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoserviceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
